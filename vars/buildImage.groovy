95c4054065f72936e2593cd2733606a7d886a2a5
macbook <gagat@tut.by>
2021-10-05 22:38:36 +0300

    revert changes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,10 @@
 #!/usr/bin/env groovy
 
 def call(String imageName){
- return new Docker(this).buildDockerImage(imageName)
+ echo "Building the docker image"
+ withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+  sh "mvn -Pprod jib:dockerBuild -Dimage=$imageName"
+  sh "echo $PASS | docker login -u $USER --password-stdin"
+  sh "docker push $imageName"
+ }
 }
