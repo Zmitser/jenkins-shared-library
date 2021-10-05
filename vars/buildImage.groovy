ac4b9b37ef4420d45021eb324d5c2efa7478da59
macbook <gagat@tut.by>
2021-10-05 21:23:48 +0300

    Fix buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo "Building the docker image"
     withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'mvn -Pprod jib:dockerBuild -Dimage=zmitser/my-repo:1.0'
+        sh "mvn -Pprod jib:dockerBuild -Dimage=$imageName"
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push zmitser/my-repo:1.0"
+        sh "docker push $imageName"
     }
 }
